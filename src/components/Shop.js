98fabf106db9e1b0006c26b6335271faf9b25c6a
rahulkprajapti <30492946+rahulkprajapti@users.noreply.github.com>
2022-09-13 12:37:54 +0530

    add to cart app first commit

---
+++ src/components/Shop.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div>
+        <h2>Buy Addidass Shoe 50 Rs</h2>
+      <button className='btn-primary mx-2 '> - </button>
+      Add to cart
+      <button className='btn-primary mx-2'> + </button>
+    </div>
+  )
+}
+
+export default Shop
+
+
