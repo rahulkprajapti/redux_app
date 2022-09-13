a167109c763266e7b79564e878f64c1f1901f512
rahulkprajapti <30492946+rahulkprajapti@users.noreply.github.com>
2022-09-13 18:47:17 +0530

    change state using bindActionCreators

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,15 +1,21 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { bindActionCreators } from 'redux';
 import { actionCreators } from '../state'
 
 const Shop = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const balance = useSelector(state=>state.amount);
+  const {withDrawMoney,depositMoney} =bindActionCreators(actionCreators,dispatch)
   return (
     <div>
         <h2>Deposit/ Withdraw Money</h2>
-      <button className='btn-primary mx-2' onClick={()=>{dispatch(actionCreators.withDrawMoney(100))}}> - </button>
+      {/*<button className='btn-primary mx-2' onClick={()=>{dispatch(actionCreators.withDrawMoney(100))}}> - </button>
       Update Balance
-      <button className='btn-primary mx-2' onClick={()=>{dispatch(actionCreators.depositMoney(100))}}> + </button>
+  <button className='btn-primary mx-2' onClick={()=>{dispatch(actionCreators.depositMoney(100))}}> + </button>*/}
+  <button className='btn-primary mx-2' onClick={()=>{withDrawMoney(100)}}> - </button>
+      Update Balance: {balance}
+      <button className='btn-primary mx-2' onClick={()=>{depositMoney(100)}}> + </button>
     </div>
   )
 }
